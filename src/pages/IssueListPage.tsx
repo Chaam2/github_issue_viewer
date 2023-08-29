e36d622df0f13dc35ee3f6aea77b2e90d4b5700c
cham2 <dearcham2@gmail.com>
2023-08-30 01:45:59 +0900

    Feat : IssueListPage의 5번째 셀 마다 광고 이미지 출력

--- src/pages/IssueListPage.tsx
+++ src/pages/IssueListPage.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { getIssueList } from '../api/issue';
 import { IIssueList } from '../types/issueListType';
 
@@ -6,7 +7,7 @@ const IssueListPage = () => {
   const [issueList, setIssueList] = useState<IIssueList[]>([]);
 
   const getIssueListData = async () => {
-    const perPage = 10;
+    const perPage = 20;
     const page = 1;
     //@ts-ignore
     const IssueListData: IIssueList[] = await getIssueList(perPage, page);
@@ -18,18 +19,42 @@ const IssueListPage = () => {
   }, []);
 
   return (
-    <>
+    <ul>
       {issueList
-        ? issueList.map(issue => {
+        ? issueList.map((issue, index) => {
             return (
-              <div key={issue.number}>
-                {issue.number}/{issue.title}/{issue.user.login}/{issue.created_at}/{issue.comments}
-                🔚
-              </div>
+              <Fragment key={issue.number}>
+                {index !== 0 && index % 4 === 0 && (
+                  <li>
+                    <Link to="https://www.wanted.co.kr/">
+                      <img
+                        src="https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Fimages%2Fuserweb%2Flogo_wanted_black.png&w=110&q=100"
+                        alt="광고 이미지"
+                      />
+                    </Link>
+                  </li>
+                )}
+                <li>
+                  <Link to={`/issue/${issue.number}`}>
+                    {issue.number}
+                    <br />
+                    {issue.title}
+                    <br />
+                    {issue.user.login}
+                    <br />
+                    {issue.created_at}
+                    <br />
+                    {issue.comments}
+                    <br />
+                    🔚
+                  </Link>
+                </li>
+              </Fragment>
             );
           })
         : null}
-    </>
+      <div style={{ backgroundColor: 'yellow' }}>end</div>
+    </ul>
   );
 };
 
